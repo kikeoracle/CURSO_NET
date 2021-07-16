8a05ff30fa9bac00226d837dc3d9e6126c872865
Modesto <modesto@sticc.com>
2021-07-16 16:23:43 +0200

    Insertado Enunciado del ejercicio

--- EjercicioConsolaBanco/EjercicioConsolaBanco/Program.cs
+++ EjercicioConsolaBanco/EjercicioConsolaBanco/Program.cs
@@ -8,6 +8,9 @@ namespace EjercicioConsolaBanco
 {
     class Program
     {
+        //Programa para gestionar un banco
+        //Se desea crear una clase tipo 
+        //cuenta para gestionar
         static void Main(string[] args)
         {
         }
